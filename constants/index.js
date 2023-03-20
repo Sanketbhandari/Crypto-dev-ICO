c057c07957d9f4bc19c5d664c9b9afbfe7fc0a20
Sanket Bhandari <sanketbhandari22@gmail.com>
2023-03-20 10:32:21 +0530

    ICO project updated

---
+++ constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that was deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x6BECe9af6d14f014d1b5081Fc0B30E5Ac15f81a5";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
